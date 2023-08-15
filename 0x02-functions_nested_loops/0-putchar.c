9c1c5687e6d2d61b603c542eeaf03b4fbaa47b9b
SoriaAbd <soriaabd1994@gmail.com>
2023-08-15 07:25:22 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,10 @@
-#include <main.h>
+#include "main.h"
 /**
- * main -  prints _putchar
- * Return:Always 0 (Success)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
+
 int main(void)
 {
 _putchar('_');
