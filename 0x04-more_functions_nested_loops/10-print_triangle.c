5eaf4cd5b90638dff862074b3103398a4073d8ae
SoriaAbd <soriaabd1994@gmail.com>
2023-08-18 08:12:08 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,22 +8,24 @@ void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		_putchar ('\n');
+		_putchar('\n');
 	}
 	else
 	{
 		int i, j;
 
-		for (i = 0 ; i <= size ; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = i ; j < size ; j++)
+			for (j = i; j < size; j++)
 			{
-				_putchar (' ');
+				_putchar(' ');
 			}
-			for (j = 1 ; j <= i ; j++)
+
+			for (j = 1; j <= i; j++)
 			{
-				_putchar ('#');
+				_putchar('#');
 			}
+
 			_putchar('\n');
 		}
 	}
