ced2a6fd2479c254165ce060493b24f5d055aa61
SoriaAbd <soriaabd1994@gmail.com>
2023-08-22 16:39:59 -0700

    1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,25 +9,29 @@
 
 int _atoi(char *s)
 {
-	int i, d, len, n, f, di;
+	int i, d, n, len, f, digit;
 
-	len = strlen(s) + 1;
 	i = 0;
 	d = 0;
 	n = 0;
+	len = 0;
 	f = 0;
-	di = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
 
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			di = s[i] - '0';
+			digit = s[i] - '0';
 			if (d % 2)
-				di = -d;
-			n = n * 10 + di;
+				digit = -digit;
+			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
@@ -35,7 +39,9 @@ int _atoi(char *s)
 		}
 		i++;
 	}
+
 	if (f == 0)
 		return (0);
+
 	return (n);
 }
