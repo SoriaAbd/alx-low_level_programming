13f158912fa5c920d27e27dbe2b5c66c7c4f9253
SoriaAbd <soriaabd1994@gmail.com>
2023-09-01 15:32:21 -0700

    print file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program that prints its name
+ * @argc:number of argu
+ * @argv:array of argu
+ * Return:Always 0 (Success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return(0);
+}
