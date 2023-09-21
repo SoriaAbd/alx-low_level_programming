7cd8374faba084d0428172c3ceaecd91979d9110
SoriaAbd <soriaabd1994@gmail.com>
2023-09-21 11:50:54 -0700

    free linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_list - function that frees a list_t list.
+ * @head:pointer to our list
+ * Return:void
+ * soria
+ */
+
+void free_list(list_t *head)
+{
+	list_t *s;
+
+	while (head == NULL)
+	{
+		s = head->next;
+		free(head->str);
+		free(head);
+		head = s;
+	}
+}
