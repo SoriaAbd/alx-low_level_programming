c8f64d09ba41cd47a9fc246c8fdd9a9349196833
SoriaAbd <soriaabd1994@gmail.com>
2023-09-22 03:55:46 -0700

    assmbly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,10 @@
-global start
+global main
+extern printf
 
-section.text
-start:
-mov rax, 1 ; write(
-mov rdi, 1 ; STDOUT_FILENO,
-mov rsi, msg ; "Hello, Holberton\n"
-mov rdx, msglen ; sizeof("Hello, Holberton\n")
-syscall  ; );
-
-mov rax, 60 ;exit(
-mov rdi, 0 ; EXIT_SUCCESS
-sysy call ; );
-
-section.rodata
-msg: db "Hello, Holberton" , 10
-msglen: equ $ - msg
+main:
+mov edi, format
+xor eax, eax
+call printf
+mov eax, 0
+ret
+format:db 'Hello, Holberton\n',0
