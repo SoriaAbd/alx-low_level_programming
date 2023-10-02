1561088b92e2962d1f88b8ac18ce25c50d34cfe9
SoriaAbd <soriaabd1994@gmail.com>
2023-10-02 10:35:06 -0700

    apppend text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	op = open(filename, O_WRONLY | O_APPEND);
 	write_t = write(op, text_content, length);
 
-	if (op == -1 || write_t)
+	if (op == -1 || write_t == -1)
 		return (-1);
 
 	close(op);
